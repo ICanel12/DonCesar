7735bfe16eeeb340d564ba8ffda0f628f28675e6
Ivan Canel <ivan.canel12@gmail.com>
2020-08-18 08:42:19 -0600

    design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,39 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'help',
+    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'donation',
+    loadChildren: () => import('./pages/donation/donation.module').then( m => m.DonationPageModule)
+  },
+  {
+    path: 'participation',
+    loadChildren: () => import('./pages/participation/participation.module').then( m => m.ParticipationPageModule)
+  },
+  {
+    path: 'invite',
+    loadChildren: () => import('./pages/invite/invite.module').then( m => m.InvitePageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/participation/participation.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participation',
+  templateUrl: './participation.page.html',
+  styleUrls: ['./participation.page.scss'],
+})
+export class ParticipationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
